abf6617fff9a03fd6b2dd42bf632497b23c89bc8
joymar300 <74611985+joymar300@users.noreply.github.com>
2024-12-10 18:08:46 -0500

    AAA

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,10 +3,10 @@ import ogImageSrc from "@images/social.png";
 export const SITE = {
   title: "EMA",
   tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
+  description: "EMA offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
+  description_short: "EMA ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
   url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  author: "TG CONPOMANY SA WEB",
 };
 
 export const SEO = {
@@ -34,6 +34,6 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
-  image: ogImageSrc,
+  description: "Equip your projects with EMA top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  // image: ogImageSrc,
 };
